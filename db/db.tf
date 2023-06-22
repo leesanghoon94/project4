39da8ac1720cfdc447d09764c0e1eb8859bd608b
devops-KJH <jhcode93@gmail.com>
2023-06-22 17:21:10 +0900

    update: db.tf and monitoring

--- db/db.tf
+++ db/db.tf
@@ -2,6 +2,18 @@ provider "aws" {
     region = "ap-northeast-2"
 }
 
+resource "aws_db_subnet_group" "default" {
+    name       = "team1_database_subnet_group"
+    subnet_ids = [
+        "subnet-093bc1cebb91c00f9",
+        "subnet-001d2da3df3529a3f"
+    ]
+
+    tags = {
+        Name = "team1 database subnet group"
+    }
+}
+
 resource "aws_db_instance" "default" {
     allocated_storage    = 20
     storage_type         = "gp2"
@@ -11,6 +23,8 @@ resource "aws_db_instance" "default" {
     username             = "root"
     password             = "12345678"
     parameter_group_name = "default.mysql8.0"
+    db_subnet_group_name = aws_db_subnet_group.default.name
     skip_final_snapshot  = true
-    publicly_accessible  = true
+    publicly_accessible  = false  # Since it is in private subnet
+    vpc_security_group_ids = ["sg-0dbdd2082538975ba"]  # Replace this with your VPC's security group ID
 }

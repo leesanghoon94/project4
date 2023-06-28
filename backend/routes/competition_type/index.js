abc8b4a29bbe83ac293e154a5ad80d18767715a8
zipkero <zipkero@gmail.com>
2023-06-28 10:28:30 +0900

    병합충돌 수정

--- backend/routes/competition_type/index.js
+++ backend/routes/competition_type/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use strict";
 
 module.exports = async function (fastify, opts) {
@@ -13,35 +12,5 @@ module.exports = async function (fastify, opts) {
     } catch (error) {
       reply.code(503).send(error);
     }
-=======
-'use strict';
-const mysql = require('mysql');
-
-const connection = mysql.createConnection({
-  host: 'terraform-20230619024655981100000001.cxamxtdxagfz.ap-northeast-2.rds.amazonaws.com',
-  user: 'root',
-  password: '12345678',
-  database: 'RECORD'
-});
-
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to MySQL database:', err);
-    process.exit(1);
-  }
-  console.log('Connected to MySQL database');
-});
-
-module.exports = async function(fastify, opts) {
-  fastify.get('/competition_type', (request, reply) => {
-    connection.query('SELECT * FROM competition_type', (err, results) => {
-      if (err) {
-        console.error('Error executing MySQL query:', err);
-        reply.status(500).send('Error executing query');
-        return;
-      }
-      reply.send(results);
-    });
->>>>>>> eeb0d36af23f1788ec7ed1ebae670f5fec5490af
   });
 };
--- backend/routes/root.js
+++ backend/routes/root.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use strict";
 
 module.exports = async function (fastify, opts) {
@@ -8,12 +7,3 @@ module.exports = async function (fastify, opts) {
       .send({ message: "마라톤대회 기록관리 시스템에 오신걸 환영합니다." });
   });
 };
-=======
-'use strict';
-
-module.exports = async function(fastify, opts) {
-  fastify.get('/', (request, reply) => {
-    reply.send({message: '마라톤대회 기록관리 시스템에 오신걸 환영합니다.'});
-  });
-}
->>>>>>> eeb0d36af23f1788ec7ed1ebae670f5fec5490af
